5ae6bd5629357223efa19de245dc2c3446e0e72c
eddyseed <randomboiii069@gmail.com>
2025-05-11 17:37:08 +0530

    feat: Implement logout functionality and enhance loading state handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
+'use client'
+import React, { useEffect, useState } from 'react';
+import { Button } from "@/components/ui/button"
+import { useAuth } from '@/context/AuthContext';
 const Home: React.FC = () => {
+  const { isLoggedIn, loading } = useAuth();
+  const [clientSide, setClientSide] = useState(false);
+  const handleLogOut = async (e:any) => {
+    e.preventDefault();
+    // To implement logout feature
+  }
+  useEffect(() => {
+    setClientSide(true);
+  }, []);
+  if (!clientSide || loading) return <p>Loading...</p>;
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
-      Logout
-      </button>
+    <div className="flex items-center justify-center h-screen" onClick={(e) => {handleLogOut(e)}}>
+      {isLoggedIn ? <Button variant="destructive">Logout</Button> : (
+        <div>
+          <h1>Please log in</h1>
+        </div>
+      )}
     </div>
   );
 };
