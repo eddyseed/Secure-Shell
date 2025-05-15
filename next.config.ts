356cd5ca06debcefd72845c9061615b659808182
eddyseed <randomboiii069@gmail.com>
2025-05-15 23:08:18 +0530

    feat: Configure image domains for Next.js application (Development)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'ciim-public-media-s3.s3.eu-west-2.amazonaws.com',
+      'i.pinimg.com',
+    ],
+  },
 };
 
 export default nextConfig;
